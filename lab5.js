b88c56ee41e7f750633833cf2572d860435bd0db
visserjackson-code <visserjackson@gmail.com>
2026-01-07 12:45:35 -0500

    Create lab5.js

---
+++ lab5.js
@@ -0,0 +1,11 @@
+// For understanding more about arrays, try creating an array that has 5 elements.
+let arr = [10, 20, 30, 40, 50];
+// Replace the value of the element at position 1 and 4.
+arr[1] = 25;
+arr[4] = 55;
+// Add a new element to the beginning of the array
+arr.unshift(5);
+// Remove the element at the end of the array
+arr.pop();
+// Print the array to the console.
+console.log(arr);
